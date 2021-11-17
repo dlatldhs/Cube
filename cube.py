2957bd784cbe8c1f73035079382e3bd82a6494bf
dlatldhs <sionzz1713@gmail.com>
2021-11-17 22:14:36 +0900

    색 다르게 넣어주기

--- cube.py
+++ cube.py
@@ -3,14 +3,14 @@ from ursina import vec3
 
 app = Ursina()
 
-Entiti = Entity()
+Entiti = Entity(enabled=True)# enabled 활성화 시키는거 True 하면 보임 Flase 하면 안보임 나중에 객체화 할 예정
 #오른쪽 면 
 cube_slice= Entity(
     parent=Entiti,# 종류???
     model='plane',# 모양
     origin_y=-.5,# size
     texture='white_cube',# 모양 텍스쳐
-    color=color.yellow# 색깔
+    color=color.white# 색깔
     )
 
 #------큐브-조각-구성-코드------------------------------------
@@ -18,18 +18,22 @@ cube_slice= Entity(
 # 1 화면 x y z
 cube_slice.look_at(Vec3(1,0,0),'up')
 
-# 2 화면 x y z
-cube_slice= Entity(parent=Entiti,model='plane',origin_y=-.5,texture='white_cube',color=color.yellow)
+# 2 화면 x y z 
+cube_slice= Entity(parent=Entiti,model='plane',origin_y=-.5,texture='white_cube',color=color.red)
 cube_slice.look_at(Vec3(-1,0,0),'up') 
 
 # 3 화면 x y z
+cube_slice= Entity(parent=Entiti,model='plane',origin_y=-.5,texture='white_cube',color=color.green)
 cube_slice.look_at(Vec3(0,0,1),'up')
 
 # 4 화면 x y z
+cube_slice= Entity(parent=Entiti,model='plane',origin_y=-.5,texture='white_cube',color=color.yellow)
 cube_slice.look_at(Vec3(0,0,-1),'up')
 
 # top and bottom
+cube_slice= Entity(parent=Entiti,model='plane',origin_y=-.5,texture='white_cube',color=color.blue)
 cube_slice.look_at(Vec3(0,1,0),'up')# top
+cube_slice= Entity(parent=Entiti,model='plane',origin_y=-.5,texture='white_cube',color=color.pink)
 cube_slice.look_at(Vec3(0,-1,0),'up')# bottom
 
 #카메라(이게 있어야지 볼 수 있음 3차원)
